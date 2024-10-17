c4993db1f879a63dc583eca33745fc4930a77c7d
adeshazer123 <adeshazer@wisc.edu>
2024-10-16 23:14:57 -0500

    Update move method.

--- src/pymodaq_plugins_optosigma/hardware/Sbis26_VISADriver.py
+++ src/pymodaq_plugins_optosigma/hardware/Sbis26_VISADriver.py
@@ -9,7 +9,7 @@ logger = set_logger(get_module_name(__file__))
 class SBIS26VISADRIVER():
     """ VISA class driver for the OptoSigma stage SBIS26. """
     def __init__(self, baud_rate = 38400, **kwargs):
-        rm = pyvisa.ResourceManager()
+        self.rm = pyvisa.ResourceManager()
         kwargs.setdefault('read_termination', '\r\n')
         self._stage = None
         self.rsrc_list = rsrc_list 
@@ -21,7 +21,7 @@ class SBIS26VISADRIVER():
         self.ch_2 = config.CHANNELS[1]
         self.ch_2 = config.CHANNELS[2]
         self.visa_address = 'ASRL/dev/ttyUSB0::INSTR'
-        self._stage = rm.open_resource(self.visa_address, 
+        self._stage = self.rm.open_resource(self.visa_address, 
                                     'read_termination', '\r\n',
                                       baud_rate = 38400)
         self.stage = self.connect_to_stage(self.visa_address)
@@ -32,13 +32,35 @@ class SBIS26VISADRIVER():
 
         self._stage.write("#CONNECT:")
         return self._stage.read()
+    def count_devices(self):
+        number = self._stage.ask("CONNECT?")
+        return number
+    
     def read(self):
         msg = super().read() #check for errors that have occured
         if msg[-1] == "NG":
             print("Error")
         return msg 
+    
     def get_position(self): 
         position = self._stage.read("Q:D,{channel}")
+        return position
+    def move(self, position):
+        pos_min = -134217728
+        pos_max = 134217727
+        if position >= pos_min and position <= pos_max: 
+            if position >= 0:
+                self._stage.write("A:D,{channel}," + f"{position}")
+            else:
+                self._stage.write("A:D,{channel}," + f"{position}")
+        else: 
+            if position >= 0:
+                position = pos_max
+                self._stage.write("A:D,{channel}," + f"{position}")
+            else: 
+                position = pos_min
+                self._stage.write("A:D,{channel}," + f"{position}")
+        self.wait_for_ready()
         return position
     def home(self): 
         """ Sends the stage to the home positio."""
