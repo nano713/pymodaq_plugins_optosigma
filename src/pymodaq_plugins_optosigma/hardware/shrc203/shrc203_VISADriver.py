e34bca46959e96832e7dda03dabb6b5abbb646c7
adeshazer123 <adeshazer@wisc.edu>
2024-10-25 18:45:59 -0500

    Delete _init_hardware

--- src/pymodaq_plugins_optosigma/hardware/shrc203/shrc203_VISADriver.py
+++ src/pymodaq_plugins_optosigma/hardware/shrc203/shrc203_VISADriver.py
@@ -23,31 +23,3 @@ class SHRC203VISADriver:
         self.rsrc_name = rsrc_name
         self.instr = ""
         self.configured_modules = {}
-
-    def init_hardware(self):
-        """Initialize the selected VISA resource
-
-        :param pyvisa_backend: Expects a pyvisa backend identifier or a path to the visa backend dll (ref. to pyvisa)
-        :type pyvisa_backend: string
-        """
-        # Open connexion with instrument
-        rm = visa.highlevel.ResourceManager()
-        logger.info("Resources detected by pyvisa: {}".format(rm.list_resources(query='?*')))
-        try:
-            self._instr = rm.open_resource(self.rsrc_name,
-                                           write_termination="\n",
-                                           read_termination="\n",
-                                           )
-            self._instr.timeout = 10000
-            # Check if the selected resource match the loaded configuration
-            model = self.get_idn()[32:36]
-            if "21" not in model:
-                logger.warning("Driver designed to use Optosigma SHRC2100, not {} model. Problems may occur.".format(model))
-            for instr in config["Optosigma", "SHRC2100"]:
-                if type(config["Optosigma", "SHRC2100", instr]) == dict:
-                    if self.rsrc_name in config["Optosigma", "SHRC2100", instr, "rsrc_name"]:
-                        self.instr = instr
-            logger.info("Instrument selected: {} ".format(config["Optosigma", "SHRC2100", self.instr, "rsrc_name"]))
-            logger.info("Optosigma model : {}".format(config["Optosigma", "SHRC2100", self.instr, "model_name"]))
-        except visa.errors.VisaIOError as err:
-            logger.error(err)
