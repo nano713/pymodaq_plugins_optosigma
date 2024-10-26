79a11aa4be43c18f33dbb222f13bf70d38e6a2f7
adeshazer123 <adeshazer@wisc.edu>
2024-10-25 21:32:23 -0500

    Add move and loop methods

--- src/pymodaq_plugins_optosigma/hardware/shrc203/shrc203_VISADriver.py
+++ src/pymodaq_plugins_optosigma/hardware/shrc203/shrc203_VISADriver.py
@@ -8,13 +8,7 @@ class SHRC203VISADriver:
     """
     Class to handle the communication with the Optosigma SHRC203 controller using the VISA protocol.
     """
-    # List the Optosigma instruments the user has configured from the .toml configuration file
-    list_instruments = {}
-    for instr in config["Optosigma", "SHRC2100"].keys():
-        if "INSTRUMENT" in instr:
-            list_instruments[instr] = config["Optosigma", "SHRC2100", instr, "rsrc_name"]
-    logger.info("Configured instruments: {}".format(list(list_instruments.items())))
-
+    
     def __init__(self, rsrc_name):
         """
         Initialize the communication with the controller.
@@ -23,3 +17,44 @@ class SHRC203VISADriver:
         self.rsrc_name = rsrc_name
         self.instr = ""
         self.configured_modules = {}
+    
+    def open_connection(self): #probably don't need this
+        """
+        Open the connection with the controller.
+        """
+        try:
+            self._instr = visa.ResourceManager().open_resource(self.rsrc_name)
+            self.instr = self._instr.query("*IDN?")
+            logger.info(f"Connection to {self.instr} successful")
+        except Exception as e:
+            logger.error(f"Error connecting to {self.rsrc_name}: {e}")
+    
+    def set_loop(self,channel, loop):
+        """
+        Open the loop of the specified channel.
+        1: Open loop
+        0: Close loop
+        """
+
+        self._instr.write(f":F{channel}:" + f"{loop}")
+        logger.info(f"Channel {channel} loop set to {loop}")
+    def get_loop(self,channel): 
+        """
+        Get the loop status of the specified channel."""
+        loop = self._instr.query(f"?:F{channel}")
+        logger.info(f"Channel {channel} loop status: {loop}")
+        return loop
+    
+    def move(self,position, channel):
+        """
+        Move the specified channel to the position. 
+        """
+        if position >= 0:
+            self._instr.write(f"A:{channel}:" + f"+P{position}")
+        else:
+            self._instr.write(f"A:{channel}:" + f"-P%{abs(position)}")
+
+
+
+
+        
