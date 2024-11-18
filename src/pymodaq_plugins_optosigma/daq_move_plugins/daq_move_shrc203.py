44d253df9cc2bc5646f5d1c6828387921b2a4a95
adeshazer123 <adeshazer@wisc.edu>
2024-11-18 07:18:32 -0600

    Update SHRC203 docstrings

--- src/pymodaq_plugins_optosigma/daq_move_plugins/daq_move_shrc203.py
+++ src/pymodaq_plugins_optosigma/daq_move_plugins/daq_move_shrc203.py
@@ -9,17 +9,10 @@ from pymodaq.utils.logger import set_logger, get_module_name
 logger = set_logger(get_module_name(__file__))
 
 class DAQ_Move_SHRC203(DAQ_Move_base):
-    """Instrument plugin class for an actuator.
+    """SHRC203 3 Axis Stage Controller plugin class
 
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
     DAQ_Move_base. It makes a bridge between the DAQ_Move module and the Python wrapper of a particular instrument.
-
-    TODO Complete the docstring of your plugin with:
-        * The set of controllers and actuators that should be compatible with this instrument plugin.
-        * With which instrument and controller it has been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
 
     Attributes:
     -----------
@@ -27,10 +20,8 @@ class DAQ_Move_SHRC203(DAQ_Move_base):
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
 
-    # TODO add your particular attributes here if any
     """
-
-
+    
     is_multiaxes = True 
     _axis_names: Union[List[str], Dict[str, int]] = {"X": 1, "Y": 2, "Z": 3}
     _controller_units: Union[str, List[str]] = SHRC203.default_units 
