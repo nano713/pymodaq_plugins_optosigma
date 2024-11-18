fb09c7f68782c2e9891e43d8450e7bc89aa608f7
adeshazer123 <adeshazer@wisc.edu>
2024-11-18 07:17:42 -0600

    Update SBIS26 docstrings

--- src/pymodaq_plugins_optosigma/daq_move_plugins/daq_move_SBIS26.py
+++ src/pymodaq_plugins_optosigma/daq_move_plugins/daq_move_SBIS26.py
@@ -7,25 +7,16 @@ from pymodaq_plugins_optosigma.hardware.sbis26_VISADriver import SBIS26VISADrive
 logger = logging.getLogger(__name__)
 
 class DAQ_Move_SBIS26(DAQ_Move_base):
-    """ Instrument plugin class for an actuator.
+    """ SBIS26 Driver Integrated Motorized Stage plugin class
 
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
     DAQ_Move_base. It makes a bridge between the DAQ_Move module and the Python wrapper of a particular instrument.
-
-    TODO Complete the docstring of your plugin with:
-        * The set of controllers and actuators that should be compatible with this instrument plugin.
-        * With which instrument and controller it has been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
 
     Attributes:
     -----------
     controller: object
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
-         
-    # TODO add your particular attributes here if any
 
     """
     
