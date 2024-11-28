0d83a57354b62a86bafdae76b6842214c4339909
adeshazer123 <adeshazer@wisc.edu>
2024-11-27 22:05:37 -0600

    Update params and methods

--- src/pymodaq_plugins_optosigma/daq_move_plugins/daq_move_GSC.py
+++ src/pymodaq_plugins_optosigma/daq_move_plugins/daq_move_GSC.py
@@ -1,15 +1,14 @@
 from typing import Union, List, Dict
+
 from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_parameters_fun, main, DataActuatorType, \
-    DataActuator
-from pymodaq.utils.daq_utils import ThreadCommand
+    DataActuator  
+from pymodaq.utils.daq_utils import ThreadCommand  
 from pymodaq.utils.parameter import Parameter
-from pymodaq_plugins_optosigma.hardware.rmc_VISADriver import RMCVISADriver
-from pymodaq.utils import logger
+from pymodaq_plugins_optosigma.hardware.gsc_VISADriver import GSC
 
-
-class DAQ_Move_RMC(DAQ_Move_base):
-    """ RMC-02C 2 Axis Controller plugin class
-
+class DAQ_Move_GSC(DAQ_Move_base):
+    """ GSC-02C 2 Axis Controller plugin class 
+    
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
     DAQ_Move_base. It makes a bridge between the DAQ_Move module and the Python wrapper of a particular instrument.
 
@@ -19,20 +18,24 @@ class DAQ_Move_RMC(DAQ_Move_base):
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
 
-    """
-    is_multiaxes = True
-    _axis_names: Union[List[str], Dict[str, int]] = {"X": 1, "Y": 2}
-    _controller_units: Union[str, List[str]] = RMCVISADriver.default_units
-    _epsilon: Union[float, List[float]] = 0.001
-    data_actuator_type = DataActuatorType.DataActuator
+    """  
+    is_multiaxes = True 
+    _axis_names: Union[List[str], Dict[str, int]] = {'Axis1':1, 'Axis2':2} 
+    _controller_units: Union[
+        str, List[str]] = GSC.default_units  
+    _epsilon: Union[
+        float, List[float]] = 1  #0.1?  
+    data_actuator_type = DataActuatorType.DataActuator 
 
     params = [
-                 {"title": "Instrument Address", "name": "visa_name", "type": "str", "value": "ASRL7::INSTR"},
-                 {"title": "Speed", "name": "speed", "type": "int", "value": 8}
+                 {"title": "Instrument Address", "name": "visa_name", "type": "str", "value": "ASRL11::INSTR"},
+                 {"title": "Speed_ini", "name": "speed_ini", "type": "int", "value": 10000},
+                 {"title": "Speed_fin", "name": "speed_fin", "type": "int", "value": 10000},
+                 {"title": "Acceleration time", "name": "acceleration_time", "type": "int", "value": 100},
              ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
 
     def ini_attributes(self):
-        self.controller: RMCVISADriver = None
+        self.controller: GSC = None
 
     def get_actuator_value(self):
         """Get the current value from the hardware with scaling conversion.
@@ -41,14 +44,13 @@ class DAQ_Move_RMC(DAQ_Move_base):
         -------
         float: The position obtained after scaling conversion.
         """
-        pos = DataActuator(
-            data=self.controller.get_position(self.axis_value))
+        pos = DataActuator(data=self.controller.get_position(self.axis_value)) 
         pos = self.get_position_with_scaling(pos)
         return pos
 
     def close(self):
         """Terminate the communication protocol"""
-        self.controller.close()
+        self.controller.close() 
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -57,19 +59,16 @@ class DAQ_Move_RMC(DAQ_Move_base):
         ----------
         param: Parameter
             A given parameter (within detector_settings) whose value has been changed by the user
+
+        raise NotImplementedError 
+            if the parameter is not implemented
         """
 
-        if param.name() == 'speed':
-            self.controller.set_speed(self.settings["speed"], self.axis_value)
+        if param.name() == "speed_ini" or param.name() == "speed_fin" or param.name() == "acceleration_time":
+            self.controller.set_speed(self.settings["speed_ini"], self.settings["speed_fin"],
+                                      self.settings["acceleration_time"], self.axis_value)
         else:
-            pass
-
-    def set_initial_conditions(self):
-        speed = 8
-        channels = [1, 2]
-        for channel in channels:
-            self.controller.set_speed(speed, channel)
-            self.controller.home(channel)
+            raise NotImplementedError(f"Parameter {param.name()} not implemented")
 
     def ini_stage(self, controller=None):
         """Actuator communication initialization
@@ -85,14 +84,15 @@ class DAQ_Move_RMC(DAQ_Move_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        self.ini_stage_init(slave_controller=controller)
 
-        if self.is_master:
-            self.controller = RMCVISADriver(self.settings["visa_name"])
+        self.ini_stage_init(slave_controller=controller) 
+
+        if self.is_master: 
+            self.controller = GSC(self.settings["visa_name"])  
             self.controller.connect()
 
-        info = "RMC Actuator initialized"
-        initialized = True
+        info = "For once it actually worked....GSC Actuator initialized"
+        initialized = True 
         return info, initialized
 
     def move_abs(self, value: DataActuator):
@@ -120,17 +120,20 @@ class DAQ_Move_RMC(DAQ_Move_base):
         self.target_value = value + self.current_position
         value = self.set_position_relative_with_scaling(value)
 
-        self.controller.move_relative(int(value.value()), self.axis_value)
+        self.controller.move_rel(int(value.value()), self.axis_value) 
+        self.emit_status(ThreadCommand('Update_Status', ['Actuator has move to the relative target'])) 
 
     def move_home(self):
         """Call the reference method of the controller"""
 
-        self.controller.home(self.axis_value)
+        self.controller.home(self.axis_value) 
+        self.emit_status(ThreadCommand('Update_Status', ['GSC has moved to home position']))
 
     def stop_motion(self):
         """Stop the actuator and emits move_done signal"""
 
-        self.controller.stop(self.axis_value)
+        self.controller.stop(self.axis_value) 
+        self.emit_status(ThreadCommand('Update_Status', ['GSC Actuator has stopped']))
 
 
 if __name__ == '__main__':
